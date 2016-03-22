73d46ad089c591fb6598cffab4d08afa29855852
Christopher Lloyd Beaufils <cbeaufil@student01.cse.nd.edu>
2016-03-22 15:11:19 -0400

    Updating Board files and makefile

--- Board.cpp
+++ Board.cpp
@@ -7,13 +7,13 @@
 
 using namespace std;
 
-Board::Board(int size=10) {
+Board::Board(int size) {
 	boardSize = size;
 
 	vector<char> row; // create one vector for each row
 
 	for (int i=0; i < boardSize; i++) {
-		row.push_back(O); // input the value into the row vector
+		row.push_back('O'); // input the value into the row vector
 	}
 
 	for (int i=0; i < boardSize; i++) {
@@ -21,14 +21,14 @@ Board::Board(int size=10) {
 	}		
 }
 
-ostream & operator<<(ostream & output, const Board&) {
-	for(int i=0; i < boardSize; i++) { // loop through each row
+ostream & operator<<(ostream & output, const Board& b) {
+	for(int i=0; i < b.boardSize; i++) { // loop through each row
 		output << "| ";
-                for(int j=0; j < boardSize; j++) { // loop through each column
-			output << display[i][j] << " | "; // display each value from the puzzle and format the output
+                for(int j=0; j < b.boardSize; j++) { // loop through each column
+			output << b.display[i][j] << " | "; // display each value from the puzzle and format the output
                         }
                 output << endl;
-                output << "-------------------------------------" << endl; // display a line between each row for formatting
+                output << "-----------------------------------------" << endl; // display a line between each row for formatting
         }
 	return output;
 }
